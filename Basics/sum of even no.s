55a5c007eb03da05b19d10f8730913a0bc29c0f6
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-26 23:57:07 +0530

    Create sum of even no.s

---
+++ Basics/sum of even no.s
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    
+	int i=1;
+	int ans=0;
+	int a=2;
+	while (i<=10){
+	    System.out.println(ans);
+	    i=i+1;
+	    ans=ans+a;
+	    a=a+2;
+	}
+	    
+	    
+	    
+	}
+}
