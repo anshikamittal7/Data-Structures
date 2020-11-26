a63356484b6b18f969d35dc6577430a6e2d9ccf2
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-26 23:17:22 +0530

    Create sum of 10 natural no.s

---
+++ Basics/sum of 10 natural no.s
@@ -0,0 +1,15 @@
+class GFG {
+	public static void main (String[] args) {
+	    int i=0;
+	    int ans=0;
+	    int a=1;
+	    while (i<10){
+	        i=i+1;
+	        
+	        ans=ans+a;
+	        a=a+1;
+        System.out.println(ans);
+	    }
+		
+	}
+}
