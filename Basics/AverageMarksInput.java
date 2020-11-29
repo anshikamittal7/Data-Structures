3a3959a4f5c372f8554115909d7fe2f755f30c49
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-29 15:18:17 +0530

    Create AverageMarksInput.java

---
+++ Basics/AverageMarksInput.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GFG {
+ 
+
+public static void main (String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int m1=s.nextInt();
+	    int m2=s.nextInt();
+	    int m3=s.nextInt();
+	    int sum=m1+m2+m3;
+	    
+	    System.out.println(sum/3);
+	    
+		
+	}
+}
