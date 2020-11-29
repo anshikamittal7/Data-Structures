a8b4399cfb57e0406c5bd77ac39194a01fd128c6
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-29 22:12:11 +0530

    Create ForLoopPrintOddNums.java

---
+++ Basics/ForLoopPrintOddNums.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+			int n=5;
+		for(int i=1;i<=5;i=i+1){
+		   if (i%2!=0){
+		       System.out.println(i);
+		       
+		   }
+		    
+		}
+		
+	}
+}
+	
