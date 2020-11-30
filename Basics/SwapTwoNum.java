0abadb68c37a523bfc4e50058d7e191c11c0ff2d
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-30 14:43:54 +0530

    Create SwapTwoNum.java

---
+++ Basics/SwapTwoNum.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int x=21;
+		int y=33;
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
