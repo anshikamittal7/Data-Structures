c6d47cc517e44c827793ef005f4c6cd4d26a3b57
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-30 14:23:17 +0530

    Create CheckOddEven.java

---
+++ Basics/CheckOddEven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	Scanner s=new Scanner(System.in);
+	int N=s.nextInt();
+	if (N%2==0){
+	    System.out.println("even");
+	}else{
+	    System.out.println("odd");
+	}
+	}
+}
