586ddbf737c044b4652c3cb966970194cb52845e
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-11-30 18:13:16 +0530

    Create ConcatenateStrings.java

---
+++ Basics/ConcatenateStrings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+	    String s1=s.nextLine();
+	    String s2=s.nextLine();
+	    String s3=s1+s2;
+	    System.out.println(s3);
+	    
+	}	
+}	
