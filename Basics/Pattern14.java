bbf30912ec91c4c782901a1acca2a3662056f491
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-12-05 14:04:07 +0530

    Create Pattern14.java

---
+++ Basics/Pattern14.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    Scanner s =new Scanner(System.in);
+	    int n=s.nextInt();
+	    int i=1;
+	    while (i<=n){
+	       int space=1;
+	       while(space<=n-i){
+	       System.out.print(" ");
+	       space++;
+	        }
+	        int j=1;
+	        while(j<=i){
+	            System.out.print("*");
+	            j++;
+	        }
+	        System.out.println();
+	        i++;
+	    }
+	}
+}
