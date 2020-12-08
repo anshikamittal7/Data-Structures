17c37f9a232db442abe308eeaa6b2d913a50b296
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-12-09 00:25:53 +0530

    Create ArraySum.java

---
+++ Basics/ArraySum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    int arr[]={1,2,34,5,67};
+	    int i=0;
+	    int sum=0;
+	    while(i<arr.length){
+	        sum=sum+arr[i];
+	        i++;
+	    }
+	    System.out.println(sum);
+	}
+}
