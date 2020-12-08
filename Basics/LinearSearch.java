8f8d00afd8ee0e445613aa264bf4ec4bc2792503
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-12-09 00:44:03 +0530

    Create LinearSearch.java

---
+++ Basics/LinearSearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    int arr[]={1,2,34,5,67};
+	    Scanner s=new Scanner(System.in);
+	    int num=s.nextInt();
+	    int i=0;
+	    String pos="no";
+	    while(i<arr.length){
+	        if (arr[i]==num){
+	            pos="yes";
+	        }
+	        i++;
+	    }
+	    System.out.println(pos);
+	}
+}
