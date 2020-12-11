fc8c5a5796b1ecf20ba4c15400d1a6b1dde793c1
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-12-11 17:03:14 +0530

    Update ArraySum.java

--- Basics/ArraySum.java
+++ Basics/ArraySum.java
@@ -12,3 +12,21 @@ class GFG {
 	    System.out.println(sum);
 	}
 }
+
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int arr[]=new int[4];
+		Scanner s=new Scanner(System.in);
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+		    arr[i]=s.nextInt();
+		    sum=sum+arr[i];
+		}
+		System.out.println(sum);
+	}
+}
