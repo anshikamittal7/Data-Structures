efbbf00a203cf25c5d7a8654ab025edc54e6cff8
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2020-12-13 12:39:38 +0530

    Update ArrayCountNumOfOnesZerosTwos.java

--- Basics/ArrayCountNumOfOnesZerosTwos.java
+++ Basics/ArrayCountNumOfOnesZerosTwos.java
@@ -1,29 +1,24 @@
 /*package whatever //do not write package name here */
-
 import java.util.*;
 
 class GFG {
 	public static void main (String[] args) {
-	    int arr[]=new int[10];
+	    int arr[]=new int[8];
 	    Scanner s=new Scanner(System.in);
 	    int count=0;
+	    int count1=0;
+	    int count2=0;
 	    for (int i=0;i<arr.length;i++){
 	        arr[i]=s.nextInt();
 	        if (arr[i]==0){
 	            count=count+1;
 	        }
-	    } 
-	    int count1=0;
-	    for (int j=0;j<arr.length;j++){
-	       
-	        if (arr[j]==1){
+	         if (arr[i]==1){
 	            count1=count1+1;
-	        }
-	    }
-	     int count2=0;
-	    for (int k=0;k<arr.length;k++){
-	        
-	        if (arr[k]==2){
+	             
+	         }
+	       	        
+	       	 if (arr[i]==2){
 	            count2=count2+1;
 	        }
 	    }
@@ -31,4 +26,6 @@ class GFG {
 	    System.out.println(count1);
 	    System.out.println(count2);
 	}
+  	    
 }
+
