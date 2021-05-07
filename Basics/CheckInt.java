d7af71697bf04a4b10ca448ddd6093f01af682bf
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-05-07 18:20:36 +0530

    Create CheckInt.java

---
+++ Basics/CheckInt.java
@@ -0,0 +1,19 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+		    Scanner s=new Scanner(System.in);
+		    float n= s.nextFloat();
+		    int y=(int)n;
+		    if(y==n){
+		        System.out.println("interger");
+		    }else{
+		        System.out.println("non integer");
+		    }
+		    
+		    
+		    
+	}
+}
