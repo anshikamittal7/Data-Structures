ee4dcf71f1ea7d4375adc8de2c8a4617f4b6f7ce
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-09-15 19:58:41 +0530

    Update ArrayTripletSum.java

--- Basics/ArrayTripletSum.java
+++ Basics/ArrayTripletSum.java
@@ -1,17 +1,15 @@
-/*package whatever //do not write package name here */
-
 import java.util.*;
 
 class GFG {
 	public static void main (String[] args) {
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		int sum=s.nextInt();
 		int arr[]=new int[n];
 		int count=0;
 		for(int i=0;i<arr.length;i++){
 		    arr[i]=s.nextInt();
 		}
+		int sum=s.nextInt();
 		for(int p=0;p<arr.length;p++){
 		    for(int j=p+1;j<arr.length;j++){
 		        for(int k=j+1;k<arr.length;k++){
@@ -25,3 +23,14 @@ class GFG {
 		System.out.println(count);
 	}
 }
+input:
+7
+1 2 3 4 5 6 7 
+12
+output:	
+1,4,7
+1,5,6
+2,3,7
+2,4,6
+3,4,5
+5
