2e3c3ca319f879c2ab4491cf8790396fec3560f8
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-09-20 01:57:04 +0530

    Update StringHighestOccuringCharacter.java

--- Basics/StringHighestOccuringCharacter.java
+++ Basics/StringHighestOccuringCharacter.java
@@ -86,6 +86,63 @@ class GFG {
 	}	
 }
 
-
 // input: highesthihhhh
 // output: h
+
+//ooooooooooooorrrrrrrrrrrrrrrr
+//more optimised
+
+
+import java.util.*;
+class GFG {
+    public static void sort(StringBuffer str){
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<str.length();j++){
+                char temp=str.charAt(i);
+                if(str.charAt(i)>str.charAt(j)){
+                    str.setCharAt(i,str.charAt(j));
+                    str.setCharAt(j,temp);
+                }
+                
+            }
+        }
+    }
+    public static void max(StringBuffer str){
+        int count =1;int max=0;
+        char ans=str.charAt(0);
+		for(int i=0;i<str.length();i++){
+		    count=1;
+		    char j=str.charAt(i);
+		    while(i<str.length()-1 && j==str.charAt(i+1)){
+		        count++;
+		        i++;
+		    }
+		    if(max<count){
+		        max=count;
+		        ans=str.charAt(i);
+		    }
+	    }
+		System.out.println(ans);
+    }
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		    while(t--!=0){
+		        StringBuffer str=new StringBuffer(s.next());
+        		sort(str);
+        		max(str);
+		    }
+		
+	}	
+}
+
+// input:
+// 3
+// xy
+// bbaaaccccc
+// abababaccc
+
+// output:
+// x
+// c
+// a
