636aaf15733ee5aa98f4c4d50cf889c60c3f44f4
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-09-22 11:15:49 +0530

    Update ArrayReverse.java

--- 450 Questions/ArrayReverse.java
+++ 450 Questions/ArrayReverse.java
@@ -7,10 +7,8 @@ class GFG {
             arr[i]=arr[arr.length-1-i];
             arr[arr.length-1-i]=temp;
         }
-        for(int j=0;j<arr.length;j++){
-            System.out.print(arr[j]+" ");
-        }
     }
+    
 	public static void main (String[] args) {
 		Scanner s=new Scanner(System.in);
 		int arr[]=new int[s.nextInt()];
@@ -18,5 +16,8 @@ class GFG {
 		    arr[i]=s.nextInt();
 		}
 		revarr(arr);
+		for(int j=0;j<arr.length;j++){
+            System.out.print(arr[j]+" ");
+        }
 	}
 }
