7056e088d38bcda02fc44c8c899d22275e979230
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-09-23 12:39:07 +0530

    Update ArraySelectionSort.java

--- Basics/ArraySelectionSort.java
+++ Basics/ArraySelectionSort.java
@@ -1,30 +1,28 @@
-/*package whatever //do not write package name here */
-
 import java.util.*;
 
 class GFG {
+    public static void ssort(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            int index=i;
+            for(int j=i;j<arr.length;j++){
+                if(arr[j]<arr[index]){
+                    index=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[index];
+            arr[index]=temp;
+        }
+        
+    }
 	public static void main (String[] args) {
-		// selection sort;
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		int arr[]=new int[n];
 		for(int i=0;i<arr.length;i++){
 		    arr[i]=s.nextInt();
-		    System.out.print(arr[i]+" ");
 		}
-		System.out.println();
-		for(int i=0;i<arr.length;i++){
-		    int max=arr[i];int maxindex=i;
-		    for(int j=i+1;j<arr.length;j++){
-		        if(arr[j]>max){
-		            max=arr[j];
-		            maxindex=j;
-		        }
-		    }
-		    int temp=arr[i];
-		    arr[i]=arr[maxindex];
-		    arr[maxindex]=temp;
-		}
+		ssort(arr);
 		for(int i=0;i<arr.length;i++){
 		    System.out.print(arr[i]+" ");
 		}
