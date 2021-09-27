8b66bda1fa3e24d2009245f2cd838c6b94add38c
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-09-27 22:37:59 +0530

    Update StringMinLenWord.java

--- Basics/StringMinLenWord.java
+++ Basics/StringMinLenWord.java
@@ -1,39 +1,30 @@
 import java.util.*;
 
 class GFG {
-    public static void rotate(int arr[],int n){
-        int arr1[]=new int[n];
-        int arr2[]=new int[arr.length - n];
-        for(int i=0;i<n;i++){
-            arr1[i]=arr[i];
-        }
-        for(int j=n;j<arr.length;j++){
-            arr2[j-n]=arr[j];
-        }
-        int i=0,j=0;
-        while(i<arr2.length){
-            arr[i]=arr2[i++];
-        }
-        while(j<arr1.length){
-            arr[i++]=arr1[j++];
-        }
-    }
+    
 	public static void main (String[] args) {
-	    Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 	    int t=s.nextInt();
+	    s.nextLine();
 	    while(t--!=0){
-	        int num=s.nextInt();
-	        int n=s.nextInt();
-	        int arr[]=new int[num];
-    		for(int i=0;i<arr.length;i++){
-    		    arr[i]=s.nextInt();
-    		}
-    		
-    		rotate(arr,n);
-    		for(int k=0;k<arr.length;k++){
-    		    System.out.print(arr[k]+" ");
-    		}
-    		System.out.println();
+	        String str=s.nextLine();
+	        int temp=Integer.MAX_VALUE;
+	        int count=0;
+	        String ans="";
+	        for(int i=0;i<str.length();i++){
+	            count=0;
+	            String word="";
+	            while(i<str.length() && str.charAt(i)!=' ' ){
+	                count++;
+	                word+=str.charAt(i);
+	                i++;
+	            }
+	            if(count<temp){
+	                temp=count;
+	                ans=word;
+	            }
+	        }
+	        System.out.println(ans);
 	    }	
 	}
 }
