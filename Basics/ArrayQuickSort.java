41b6a76d01fdb16c67819614905faeb9a760e990
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-10-05 00:50:18 +0530

    Create ArrayQuickSort.java

---
+++ Basics/ArrayQuickSort.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    public static void swap(int arr[],int a,int b){
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low<high){
+            int pivotpos=partition(arr,low,high);
+            quickSort(arr,low,pivotpos-1);
+            quickSort(arr,pivotpos+1,high);
+        }
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<=pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,high);
+        return i+1;
+    } 
+}
