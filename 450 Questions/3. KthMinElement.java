1cd46ac40adefc2f05217d977843552ec2d2f638
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-10-06 17:48:46 +0530

    Create 3. KthMinElement.java

---
+++ 450 Questions/3. KthMinElement.java
@@ -0,0 +1,40 @@
+class Solution{
+    public static void merge(int arr[], int l, int mid, int r)
+    {
+        int i=l,j=mid+1,k=0;
+        int ans[]=new int[r-l+1];
+        while(i<=mid && j<=r){
+            if(arr[i]<arr[j]){
+                ans[k]=arr[i];i++;k++;
+            }
+            if(arr[i]>arr[j]){
+                ans[k]=arr[j];j++;k++;
+            }
+        }
+        while(i<=mid){
+            ans[k]=arr[i];i++;k++;
+        }
+        
+        while(j<=r){
+            ans[k]=arr[j];j++;k++;
+        }
+        for(int p=l,m=0;p<=r;m++,p++){
+            arr[p]=ans[m];
+        }
+    }
+    public static void mergeSort(int arr[], int l, int r)
+    {
+        int mid=(l+r)/2;
+        if(l<r){
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1,r);
+            merge(arr,l,mid,r);
+        }
+        
+    }
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        mergeSort(arr,0,arr.length-1);
+        return arr[k-1];
+    } 
+}
