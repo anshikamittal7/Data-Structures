4b623e498527b2e1b63afa9697468ed687912113
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-10-12 16:29:19 +0530

    Update StringPrintSubsequences.java

--- Basics/StringPrintSubsequences.java
+++ Basics/StringPrintSubsequences.java
@@ -32,3 +32,41 @@ class GFG {
 	}
 }
 
+
+
+//or
+
+
+import java.util.*;
+
+class GFG {
+    public static void subseq(String str,String output){
+        if(str.length()==0){
+            System.out.print(output+" ");
+            return ;
+        }
+        subseq(str.substring(1),output);
+        subseq(str.substring(1),output+str.charAt(0));
+        
+    }
+    public static void subseq(String str){
+        subseq(str,"");
+    }
+	public static void main (String[] args) {
+	Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t--!=0){
+		    String str=s.next();
+		    subseq(str);
+		    System.out.println();
+		}   
+	}
+}
+
+// input:
+// 2
+// xyz
+// abcd
+// output:
+//  z y yz x xz xy xyz 
+//  d c cd b bd bc bcd a ad ac acd ab abd abc abcd 
