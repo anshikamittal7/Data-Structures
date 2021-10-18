b6ebcd3efc83dcf0fb28354e4f0b5c409ec899dd
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-10-18 17:30:31 +0530

    Update OopsPract2.java

--- Basics/OopsPract2.java
+++ Basics/OopsPract2.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class CN{
     int real;
-    int complex;
-    public CN(int real, int complex){
+    int imaginary;
+    public CN(int real, int imaginary){
         this.real=real;
-        this.complex=complex;
+        this.imaginary=imaginary;
     } 
     
     public void print(){
-        if(this.complex>=0) System.out.println(real+" + "+complex+"i");
-        else System.out.println(real+" - "+ -complex+"i");
+        if(this.imaginary>=0) System.out.println(real+" + "+imaginary+"i");
+        else System.out.println(real+" - "+ -imaginary+"i");
     }
     public static CN sum(CN n1, CN n2){
-        CN n3=new CN(n1.real +n2.real, n1.complex+n2.complex);
+        CN n3=new CN(n1.real +n2.real, n1.imaginary+n2.imaginary);
         return n3;
     }
     public static CN product(CN n1,CN n2){
-        CN n3= new CN(n1.real*n2.real-n1.complex*n2.complex, n1.complex*n2.real +n1.real*n2.complex );
+        CN n3= new CN(n1.real*n2.real-n1.imaginary*n2.imaginary, n1.imaginary*n2.real +n1.real*n2.imaginary );
         return n3;
     }
 }
@@ -28,7 +28,7 @@ class GFG {
 		CN n1=new CN(2,1);
 		n1.print();
 		
-		CN n2=new CN(3,5);
+		CN n2=new CN(3,-5);
 		n2.print();
 		
 		CN n3=CN.sum(n1,n2);
