76a8efccf3f83a6e2a5e1cf69fd115c187751e29
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-11-08 18:24:20 +0530

    Create SelectionSort.java

---
+++ Recursion/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class GFG {
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void SelectionSort(int arr[], int i){
+        if(i == arr.length) return;
+        int k = i;
+        int min = i;
+        while(k < arr.length){
+            if(arr[k]<arr[min]){
+                min = k;
+                k++;
+            }        
+            k++;
+        }
+        swap(arr, i, min);
+        SelectionSort(arr, i+1);
+    }
+    
+	public static void main (String[] args) {
+	    int arr[] = {3,0};
+	    SelectionSort(arr,0);
+		System.out.println(Arrays.toString(arr));
+	}
+}
