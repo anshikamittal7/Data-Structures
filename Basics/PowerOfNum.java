09f26e0eb7f152a66ab6fb2ae8d519fcc0789b0a
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-11-15 23:25:53 +0530

    Update PowerOfNum.java

--- Basics/PowerOfNum.java
+++ Basics/PowerOfNum.java
@@ -23,20 +23,22 @@ class GFG {
 }
 
 
-class Solution {
-    public double power(double n,long pow){
+import java.util.*;
+
+class GFG {
+    public static double power(double n,double pow){
+        if(pow==1) return n;
         if(pow==0) return 1;
-        if(n==0 && pow==0) return 1;
-        if(n==0) return 0;
         if(pow<0){
             return power(1/n,-pow);
         }
-        double ans=power    (n*n,pow/2);
-        if(pow%2!=0){
-            ans*=n;
-        }return ans;
+        double smallans = power(n*n,pow/2);
+        if(pow%2==0){
+            return smallans;
+        }
+        return smallans*n;
     }
-    public double myPow(double x, int n) {
-        return power(x,n);
-    }
+	public static void main (String[] args) {
+	    System.out.println(power(5,4));
+	}
 }
