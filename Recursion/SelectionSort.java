a496ab80690f4c639603891d2a2bf95042df6bf2
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2021-11-19 21:28:33 +0530

    Update SelectionSort.java

--- Recursion/SelectionSort.java
+++ Recursion/SelectionSort.java
@@ -6,27 +6,27 @@ class GFG {
         arr[l] = arr[m];
         arr[m] = temp;
     }
-    public static void selection(int arr[], int i, int j,int max){
-        if(i==0){
+    public static void selection(int arr[], int i, int j, int max){
+        if(i < 0){
             return;
         }
-        if(i>j){
+        if(i>=j){
             if(arr[max]<arr[j]){
-                max=j;
-                selection(arr,i,j+1,max);
+                selection(arr,i,j+1,j);
             }
             else{
                 selection(arr,i,j+1,max);
             }
-        }else{
-            swap(arr,max,i-1);
+        }
+        else{
+            swap(arr,i,max);
             selection(arr,i-1,0,0);
         }
     }
 	public static void main (String[] args) {
 	    int arr[]={3,5,1,2,1,8,9,5,4,2,4,5,1,4,2,89,32,11,5,22,0,-21,-90,54};
 	    System.out.println(Arrays.toString(arr));
-	    selection(arr,arr.length,0,0);
+	    selection(arr,arr.length-1,0,0);
 	    System.out.println(Arrays.toString(arr));
 	}
 }
