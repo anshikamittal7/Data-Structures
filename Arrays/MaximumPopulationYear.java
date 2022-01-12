878be58e8dc5670a4d31c46bd79cf2f1c86d0196
anshikamittal7 <75077180+anshikamittal7@users.noreply.github.com>
2022-01-13 02:43:10 +0530

    Update MaximumPopulationYear.java

--- Arrays/MaximumPopulationYear.java
+++ Arrays/MaximumPopulationYear.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int bucket[]=new int[2051];
+        int bucket[]=new int[2051-1950];
         for(int i=0;i<logs.length;i++){
             for(int j=logs[i][0];j<logs[i][1];j++){
-                bucket[j]++;
+                bucket[j-1950]++;
             }
         }
-        int max = 0;
-        int k=0;
+        int max = 2050;
         for(int i=2050;i>=1950;i--){
-            if(bucket[i]>=max){
-                max = bucket[i];
-                k=i;
+            if(bucket[i-1950]>=bucket[max-1950]){
+                max = i;
             }
         }
-        return k;
+        return max;
     }
 }
