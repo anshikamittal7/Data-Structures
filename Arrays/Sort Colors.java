c617d13d1de58c09fb2663256e1a6b0a13feafbb
Anshika Mittal <anshikamittal34@gmail.com>
2022-01-17 21:10:10 +0530

    Create Sort Colors.java

---
+++ Arrays/Sort Colors.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0=0,count1=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
+            }
+            else if(nums[i]==1){
+                count1++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i<count0) nums[i]=0;
+            else if(i<count1+count0) nums[i]=1;
+            else{
+                nums[i]=2;
+            }
+        }
+    }
+}
