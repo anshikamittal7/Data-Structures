d8b437df1e778dad03ce36ee9e46f230bdd5980a
Anshika Mittal <anshikamittal34@gmail.com>
2022-01-19 13:08:45 +0530

    Update SpiralMatrixII.java

--- Arrays/SpiralMatrixII.java
+++ Arrays/SpiralMatrixII.java
@@ -1,29 +1,21 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int spiral[][]=new int[n][n];
-        int num = 1;
-        int row = n;
-        int col = n;
-        int rowStart=0;
-        int colStart=0;
-        while(num<=n*n){
-            for(int i=colStart;num<=n*n&&i<col;i++){
-                spiral[rowStart][i]=num;num++;
+        int[][] ans = new int[n][n];
+        int num=1;
+        for(int layer=0;layer<(n+1)/2;layer++){
+            for(int ptr=layer;ptr<n-layer;ptr++){
+                ans[layer][ptr]=num++;
             }
-            rowStart++;
-            for(int i=rowStart;num<=n*n&&i<row;i++){
-                spiral[i][col-1]=num;num++;
+            for(int ptr=layer+1;ptr<n-layer;ptr++){
+                ans[ptr][n-layer-1]=num++;
             }
-            col--;
-            for(int i=col-1;num<=n*n&&i>=colStart;i--){
-                spiral[row-1][i]=num;num++;
+            for(int ptr=n-2-layer;ptr>=layer;ptr--){
+                ans[n-layer-1][ptr]=num++;
             }
-            row--;
-            for(int i=row-1;num<=n*n&&i>=rowStart;i--){
-                spiral[i][colStart]=num;num++;
+            for(int ptr=n-2-layer;ptr>layer;ptr--){
+                ans[ptr][layer]=num++;
             }
-            colStart++;
         }
-        return spiral;
+        return ans;
     }
 }
